9e8cdc5b84389187ca896def5acd67c992e00747
parthgupta118 <guptaparth118@gmail.com>
2020-04-19 12:25:14 +0530

    Updates

---
+++ precache-manifest.889ed7dba83ba335f740348a8bb712db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6b49c2be97e024788cd441743e368e6",
+    "url": "/ecommerce/index.html"
+  },
+  {
+    "revision": "362c51b1009f3d418780",
+    "url": "/ecommerce/static/css/main.f29b4d6d.chunk.css"
+  },
+  {
+    "revision": "2497cb277fb05da6da33",
+    "url": "/ecommerce/static/js/2.32c886b0.chunk.js"
+  },
+  {
+    "revision": "4c796ab9273dad0140c4d6224d61af40",
+    "url": "/ecommerce/static/js/2.32c886b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "362c51b1009f3d418780",
+    "url": "/ecommerce/static/js/main.cb512f05.chunk.js"
+  },
+  {
+    "revision": "bd7f6712790c0d25507b",
+    "url": "/ecommerce/static/js/runtime-main.f2ca0e68.js"
+  }
+]);
